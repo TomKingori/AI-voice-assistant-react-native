d4a02871c325a44e1f557c3a8fe5e50f1faa2213
TomKingori <tomkingori66@gmail.com>
2023-12-20 15:46:05 +0300

    Initial app

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, Platform } from "react-native";
+import GlobalStyles from "./theme/GlobalStyles";
+import React from "react";
+
+const ios = Platform.OS == "ios";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SanderAI</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-neutral-800">
+    <SafeAreaView className={ios ? "-mb-2" : "mb-3 mt-12"}>
+        <Text className="text-white">App</Text>
+    </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
