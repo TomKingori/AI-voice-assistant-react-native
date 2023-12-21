25cef50a17bd24c54596fc7d5cf1f5d313dc0a04
TomKingori <tomkingori66@gmail.com>
2023-12-21 19:22:39 +0300

    Initial setup

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import { View, Text, SafeAreaView, Platform } from "react-native";
-import GlobalStyles from "./theme/GlobalStyles";
 import React from "react";
+import AppNavigation from "./src/navigation/AppNavigation";
 
-const ios = Platform.OS == "ios";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-neutral-800">
-    <SafeAreaView className={ios ? "-mb-2" : "mb-3 mt-12"}>
-        <Text className="text-white">App</Text>
-    </SafeAreaView>
-    </View>
+    <AppNavigation />
   );
 }
