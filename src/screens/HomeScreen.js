2e1c9562513fdb498645f83eaf297ee34d86caf9
TomKingori <tomkingori66@gmail.com>
2023-12-22 11:07:35 +0300

    homescreen UI

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,34 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import React, { useState } from "react";
 import Features from "../components/features";
+import { dummyMessages } from "../constants";
 
 const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(dummyMessages);
+  const [recording, setRecording] = useState(false);
+  const [speaking, setSpeaking] =  useState(true)
+
+  const clear = () => {
+    setMessages([])
+  }
+
+  const stopSpeaking = () => {
+    setSpeaking(false)
+  }
+
 
   return (
     <View className="flex-1 bg-white">
@@ -27,7 +46,107 @@ export default function HomeScreen() {
         </View>
 
         {/* Features || messages */}
-        {messages.length > 0 ? <View></View> : <Features />}
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{ fontSize: wp(5) }}
+              className="text-gray-700 font-semibold ml-1"
+            >
+              Assistant
+            </Text>
+            <View
+              style={{ height: hp(58) }}
+              className="bg-neutral-200 rounded-3xl p-4"
+            >
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}
+              >
+                {messages.map((message, index) => {
+                  if (message.role == "assistant") {
+                    if (message.content.includes("https")) {
+                      //Its an AI image
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{ uri: message.content }}
+                              className="rounded-2xl"
+                              resizeMode="contain"
+                              style={{ height: wp(60), width: wp(60) }}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      //Text response
+                      return (
+                        <View
+                          key={index}
+                          style={{ width: wp(70) }}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    //User input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{ width: wp(70) }}
+                          className="bg-white rounded-xl p-2 rounded-tr-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
+
+        {/* recording, stop and clear buttons */}
+        <View className="flex justify-center items-center">
+          {recording ? (
+            <TouchableOpacity>
+              <Image
+                className="rounded-full"
+                source={require("../../assets/images/voiceLoading.gif")}
+                style={{ width: hp(10), height: hp(10) }}
+              />
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity>
+              <Image
+                className="rounded-full"
+                source={require("../../assets/images/recordingIcon.png")}
+                style={{ width: hp(10), height: hp(10) }}
+              />
+            </TouchableOpacity>
+          )}
+
+          {messages.length > 0 && (
+            <TouchableOpacity
+            onPress={clear}
+             className="bg-neutral-400 rounded-3xl p-2 absolute right-10">
+              <Text className="text-white font-semibold">Clear</Text>
+            </TouchableOpacity>
+          )}
+          {speaking > 0 && (
+            <TouchableOpacity
+            onPress={stopSpeaking}
+             className="bg-red-400 rounded-3xl p-2 absolute left-10">
+              <Text className="text-white font-semibold">Stop</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </SafeAreaView>
     </View>
   );
